e319a5fcd2e2d4587ef30ad1adbf6ab8b00a7652
Alex <>
2020-12-21 15:37:24 +0300

    Add new textures image and positions of all textures

--- public/js/textures/backgroundTextures.js
+++ public/js/textures/backgroundTextures.js
@@ -1,7 +1,9 @@
 export const backgroundTextures = {
   'sky':{
-    'light-blue': [120, 80],
-    'blue': [120, 48],
-    'red': [120, 16],
+    'light-blue': [0, 64],
+    'light-blue-2': [0, 96],
+    'blue': [0, 80],
+    'red': [0, 128],
+    'black':[0,112]
   }
 }
